6e1d38aed0d5fb26b450b5c0e0a578537a048767
mehdieidi <mehdiadq@gmail.com>
2025-08-16 10:13:53 +0000

    Deploying to gh-pages from @ mehdieidi/mehdieidi.github.io@370d0537e16ce1c62114c6b1723692997278e883 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,6 +35,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/mda_distilled/";
+            },},{id: "books-serverless-development-on-aws-building-enterprise-scale-serverless-solutions",
+          title: 'Serverless Development on AWS: Building Enterprise-Scale Serverless Solutions',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/serverless_development_on_aws/";
             },},{
         id: 'social-email',
         title: 'email',
