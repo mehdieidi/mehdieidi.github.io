c0011d54c9b65c1d4a3eb807340704593d120158
mehdieidi <mehdiadq@gmail.com>
2025-10-02 11:33:59 +0000

    Deploying to gh-pages from @ mehdieidi/mehdieidi.github.io@4fc160cce5ac5d010a0e31017d845408ec492197 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,56 +30,16 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "books-artificial-intelligence-a-modern-approach",
-          title: 'Artificial Intelligence: A Modern Approach',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/ai/";
-            },},{id: "books-introduction-to-the-design-and-analysis-of-algorithms",
-          title: 'Introduction to the Design and Analysis of Algorithms',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/algorithm/";
-            },},{id: "books-an-introduction-to-formal-languages-and-automata",
-          title: 'An Introduction to Formal Languages and Automata',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/automata/";
-            },},{id: "books-c-how-to-program",
+        },{id: "books-c-how-to-program",
           title: 'C How to Program',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/c_how_to_program/";
-            },},{id: "books-compilers-principles-techniques-and-tools",
-          title: 'Compilers: Principles, Techniques, and Tools',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/compiler/";
-            },},{id: "books-computer-organization-and-design-mips-edition-the-hardware-software-interface",
-          title: 'Computer Organization and Design MIPS Edition: The Hardware/Software Interface',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/computer_organization/";
             },},{id: "books-core-java-volume-i-fundamentals",
           title: 'Core Java Volume I--Fundamentals',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/core_java/";
-            },},{id: "books-cryptography-and-network-security-principles-and-practice",
-          title: 'Cryptography and Network Security: Principles and Practice',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/cryptography/";
-            },},{id: "books-data-structures-and-algorithms-in-java",
-          title: 'Data Structures and Algorithms in Java',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/data_structures/";
-            },},{id: "books-database-system-concepts",
-          title: 'Database System Concepts',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/database/";
             },},{id: "books-digital-design",
           title: 'Digital Design',
           description: "",
@@ -90,21 +50,41 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/discrete_math/";
-            },},{id: "books-introduction-to-information-retrieval",
-          title: 'Introduction to Information Retrieval',
+            },},{id: "books-computer-organization-and-design-mips-edition-the-hardware-software-interface",
+          title: 'Computer Organization and Design MIPS Edition: The Hardware/Software Interface',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/books/information_retrieval/";
+              window.location.href = "/books/computer_organization/";
+            },},{id: "books-data-structures-and-algorithms-in-java",
+          title: 'Data Structures and Algorithms in Java',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/data_structures/";
+            },},{id: "books-introduction-to-the-design-and-analysis-of-algorithms",
+          title: 'Introduction to the Design and Analysis of Algorithms',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/algorithm/";
+            },},{id: "books-an-introduction-to-formal-languages-and-automata",
+          title: 'An Introduction to Formal Languages and Automata',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/automata/";
+            },},{id: "books-database-system-concepts",
+          title: 'Database System Concepts',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/database/";
+            },},{id: "books-compilers-principles-techniques-and-tools",
+          title: 'Compilers: Principles, Techniques, and Tools',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/compiler/";
             },},{id: "books-linux-system-programming",
           title: 'Linux System Programming',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/linux_programming/";
-            },},{id: "books-mda-distilled-principles-of-model-driven-architecture",
-          title: 'MDA Distilled: Principles of Model-Driven Architecture',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/mda_distilled/";
             },},{id: "books-computer-networking-a-top-down-approach",
           title: 'Computer Networking: A Top-Down Approach',
           description: "",
@@ -115,6 +95,26 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/os/";
+            },},{id: "books-systems-analysis-and-design",
+          title: 'Systems Analysis and Design',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/systems_design/";
+            },},{id: "books-artificial-intelligence-a-modern-approach",
+          title: 'Artificial Intelligence: A Modern Approach',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/ai/";
+            },},{id: "books-cryptography-and-network-security-principles-and-practice",
+          title: 'Cryptography and Network Security: Principles and Practice',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/cryptography/";
+            },},{id: "books-introduction-to-information-retrieval",
+          title: 'Introduction to Information Retrieval',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/information_retrieval/";
             },},{id: "books-concepts-of-programming-languages",
           title: 'Concepts of Programming Languages',
           description: "",
@@ -125,16 +125,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/se/";
+            },},{id: "books-mda-distilled-principles-of-model-driven-architecture",
+          title: 'MDA Distilled: Principles of Model-Driven Architecture',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/mda_distilled/";
             },},{id: "books-serverless-development-on-aws-building-enterprise-scale-serverless-solutions",
           title: 'Serverless Development on AWS: Building Enterprise-Scale Serverless Solutions',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/serverless_development_on_aws/";
-            },},{id: "books-systems-analysis-and-design",
-          title: 'Systems Analysis and Design',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/systems_design/";
             },},{
         id: 'social-email',
         title: 'email',
